7beec2a998425f8317a08adf3288afa0e196a576
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-07-15 02:09:35 -0500

    almost there

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 let button = document.querySelector('.button');
 let inputValue = document.querySelector('.inputValue');
-let name = document.querySelector('.name');
 let temp = document.querySelector('.temp');
 let humidity = document.querySelector('.humidity');
 let windSpeed = document.querySelector('.wind-speed');
@@ -13,36 +12,35 @@ button.addEventListener('click', getGeoApi)
 
         // Geo Api fetch
 function getGeoApi() {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${inputValue.value}&limit=5&appid=0dff19434666c0633032ec00e05dfa2a`)
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${inputValue.value}&limit=1&appid=0dff19434666c0633032ec00e05dfa2a`)
         .then(function (responseGeo) {
             return responseGeo.json();
         })
         .then(function (dataGeo){
             console.log("Current dataGeo content:")
             console.log(dataGeo)
-            console.log("Current dataGeo[0].name content:")
+            console.log("Current dataGeo[0] content:")
             console.log(dataGeo[0].name)
-            console.log("Current dataGeo[0].lat content:")
             console.log(dataGeo[0].lat)
-            console.log("Current dataGeo[0].lon content:")
             console.log(dataGeo[0].lon)
-            var name = dataGeo[0].name
+            var cityName = dataGeo[0].name
             var lat = dataGeo[0].lat
             var lon = dataGeo[0].lon
-            getWeatherApi(name,lat,lon);
+            getWeatherApi(cityName,lat,lon);
     })
 }
 
         // Weather Api Fetch
-function getWeatherApi(name,lat,lon) {
+function getWeatherApi(cityName,lat,lon) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0dff19434666c0633032ec00e05dfa2a`)
     .then(function (responseWeather) {
         return responseWeather.json();
     })
     .then(function (dataWeather) {
-        weatherResultsArray = dataWeather;
+        let name = document.getElementById('name');
         console.log("Current weatherResultsArray content:")
-        console.log(weatherResultsArray)
+        console.log(dataWeather)
+        name.innerHTML = cityName;
     })
 }
 
